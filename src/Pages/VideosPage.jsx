49ee52e21451710963427de3e0664e19655c4fb4
Hager Amr <hageramr320@gmail.com>
2025-05-17 00:13:12 +0200

    edit on contetn

--- src/Pages/VideosPage.jsx
+++ src/Pages/VideosPage.jsx
@@ -6,14 +6,6 @@ const VideosPage = () => {
   const [videos] = useState([
     {
       id: uuidv4(),
-      title: "UP",
-      url: "https://youtu.be/HWEW_qTLSEE?si=1VfBaebnsdfqrgRW",
-      duration: "2:30",
-      thumbnail:
-        "https://www.liveabout.com/thmb/Uwm6lYie5SxgyLed5ufDpFn2pxY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/up001-56a573d75f9b58b7d0dcfb06.jpg",
-    },
-    {
-      id: uuidv4(),
       title: "Oblivion: Official Teaser Trailer",
       url: "https://www.youtube.com/embed/XmIIgE7eSak",
       duration: "2:37",
